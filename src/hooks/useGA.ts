f7235d78eb9bd17a17494889f30f8562c96e2fc6
krzysztof <k.b.ziemski@gmail.com>
2021-11-08 20:02:39 +0100

    change dependencies from react-ga to ga-4-react

--- src/hooks/useGA.ts
+++ src/hooks/useGA.ts
@@ -1,13 +1,37 @@
+import GA4React from "ga-4-react";
 import { useCallback, useEffect } from "react";
-import ReactGA from "react-ga";
 import { useDispatch, useSelector } from "react-redux";
 import { setAgreeCookies } from "../store/appConfig/actions";
 import { AllSettingsStoreSelector } from "../store/appConfig/selectors";
 
-const enableGA = () => {
+const enableGA = async () => {
+  // console.log(process.env.REACT_APP_GA);
+  // if (!process.env.REACT_APP_GA) return;
+  // ReactGA.initialize(process.env.REACT_APP_GA);
+  // ReactGA.pageview(window.location.pathname);
   if (!process.env.REACT_APP_GA) return;
-  ReactGA.initialize(process.env.REACT_APP_GA);
-  ReactGA.pageview(window.location.pathname);
+
+  const ga4react = new GA4React(
+    process.env.REACT_APP_GA,
+    {
+      /* ga custom config, optional */
+    },
+    [
+      /* additional code, optional */
+    ],
+    5000 /* timeout, optional, defaults is 5000 */
+    // options /* { nonce: ['first-script-is-async','second-script'] } */
+  );
+
+  ga4react.initialize().then(
+    (ga4) => {
+      ga4.pageview("path");
+      ga4.gtag("event", "pageview", window.location.pathname); // or your custom gtag event
+    },
+    (err) => {
+      console.error("ga4react initialize error", err);
+    }
+  );
 };
 
 export const useGA = () => {
